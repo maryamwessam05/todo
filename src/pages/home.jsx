32951ce722fe1d6792be33d2f55b070d0f7280d2
maryamwessam05 <23-101015@students.eui.edu.eg>
2026-04-01 20:13:53 +0200

    tasks preview

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -22,20 +22,53 @@ const Home = () => {
     return ( 
         <>
             <main>
-                <div className="profile">
-                    <img src={profimg} alt="" />
-                    <div className="profdeets">
-                        <h3>Hey there <span>Maryam</span></h3>
-                        <p>Streaks of activity </p>
+                <div className="taskscont">
+                    <div className="profile">
+                        <img src={profimg} alt="" />
+                        <div className="profdeets">
+                            <div className="name">
+                                <h3>Hey there <span>Maryam</span></h3>
+                                <p>Streaks of activity </p>
 
-                        <div className="streak">
-                            <div className="fire">
-                                <p>5 Days in a row</p>
-                                <img src={fire} alt="" />
                             </div>
-                            <img src={bar} alt="" />
+
+                            <div className="streak">
+                                <div className="fire">
+                                    <p>5 Days in a row</p>
+                                    <img src={fire} alt="" />
+                                </div>
+                                <img src={bar} alt="" />
+                            </div>
                         </div>
                     </div>
+
+                    <div className="createtask">
+                        
+                    </div>
+
+                    <div className="tasks">
+                        <h1>Today's Tasks</h1>
+                        <div className="tasklist">
+                            {students.map((student) => (
+                                <div className="task" key={student.id} style={{ backgroundColor: student.color }}>
+                                    <div className="row1">
+                                        <div className="header">
+                                            <h3>{student.title}</h3>
+                                            <p>{student.description}</p>
+                                        </div>
+
+                                        <div className='complete'></div>
+
+                                    </div>
+                                    <div className="taskbtns">
+                                        <button className='edit'>Edit</button>
+                                        <button className='delete'>Delete</button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                 </div>
 
             </main>
